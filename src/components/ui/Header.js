04d47c26e9d0bbbc2188b5b6c93dc49956571c9e
shashankkurakula <shashankkurakula96@gmail.com>
2020-07-03 20:19:17 +0530

    final commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../img/logo.png';
+
+function Header() {
+  return (
+    <header className='center'>
+      <img src={logo} alt='logo' />
+    </header>
+  );
+}
+
+export default Header;
